01f4b99691beb81babd143a93048beb0fb8544eb
Tristan Emerson <tristan.emerson@echoman.com>
2022-01-29 15:26:42 -0500

    refactor code to remove some duplication (at the cost of other)

--- src/scoring.ts
+++ src/scoring.ts
@@ -2,21 +2,6 @@ import { Word } from './types';
 
 type Scores = { [key: string]: number };
 
-function isGreen(baseWord: string, compareWord: string, index: number): boolean {
-  return baseWord[index] == compareWord[index];
-}
-
-function isYellow(baseWord: string, compareWord: string, index: number): boolean {
-  // this will count yellows multiple times, which needs to be fixed
-  // eg if the baseWord is `eerie` and the compareWord is `steps` it will find 3 yellow matches
-  return !isGreen(baseWord, compareWord, index) && compareWord.includes(baseWord[index]);
-}
-
-// scores based on number of other words with the same letters, after removing duplicates
-function scoreWordByLetterFrequency(word: string, letterFrequencies: Scores): number {
-  return Array.from(new Set(word)).reduce((score, letter) => score + letterFrequencies[letter], 0);
-}
-
 // replace non-unique letters with underscores, eg: `eerie` => `e_ri_`
 function toUniqueLetters(word: string): string {
   const usedLetters: string[] = [];
@@ -31,6 +16,23 @@ function toUniqueLetters(word: string): string {
     }, '');
 }
 
+// a letter would result in a green tile if it is an exact match
+function isGreen(baseWord: string, compareWord: string, index: number): boolean {
+  return baseWord[index] == compareWord[index];
+}
+
+// a letter would result in a yellow tile if it is not an exact match but does exist in the word
+// duplicates are removed from the baseWord to prevent words like `eerie` to match `sheds` three times
+function isYellow(baseWord: string, compareWord: string, index: number): boolean {
+  const uniqueLetters = toUniqueLetters(baseWord);
+  return !isGreen(baseWord, compareWord, index) && compareWord.includes(uniqueLetters[index]);
+}
+
+// scores based on number of other words with the same letters, after removing duplicates
+function scoreWordByLetterFrequency(word: string, letterFrequencies: Scores): number {
+  return Array.from(new Set(word)).reduce((score, letter) => score + letterFrequencies[letter], 0);
+}
+
 // scores by most words with matching letters in the same spot, one point for each match in the word
 function scoreWordByMostGreens(word: string, dictionary: string[]): number {
   const countGreens: Function = (baseWord: string, compareWord: string) => Array
@@ -43,15 +45,17 @@ function scoreWordByMostGreens(word: string, dictionary: string[]): number {
 
 // scores by most words with matching letters in the same spot, two points per green and one point for each yellow in the word
 function scoreWordByMostGreensAndYellows(word: string, dictionary: string[]): number {
-  const countGreens: Function = (baseWord: string, compareWord: string) => Array
+  const countGreensAndYellows: Function = (baseWord: string, compareWord: string) => Array
     .from(baseWord)
-    .reduce((score, _, index) => score + (isGreen(baseWord, compareWord, index) ? 2 : 0), 0);
-  const countYellows: Function = (baseWord: string, compareWord: string) => Array
-    .from(baseWord) 
-    .map(toUniqueLetters)
-    .reduce((score, _, index) => score + (isYellow(baseWord, compareWord, index) ? 1 : 0), 0);
+    .reduce((score, _, index) => {
+      const letterPoints: number =
+        isGreen(baseWord, compareWord, index) ? 2
+        : isYellow(baseWord, compareWord, index) ? 1
+        : 0;
+      return score + letterPoints;
+    }, 0);
   return dictionary
-    .map((compareWord) => countGreens(word, compareWord) + countYellows(word, compareWord))
+    .map((compareWord) => countGreensAndYellows(word, compareWord))
     .reduce((total, score) => total + score, 0);
 }
 
